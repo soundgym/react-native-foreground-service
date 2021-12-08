383681e0c2de4302742ce445792ab2bc9476931c
StudyTae <dea972@naver.com>
2021-12-09 06:34:41 +0900

    feat : 백그라운드 부분 변경

--- android/src/main/java/com/leetaehong/foregroundservice/NotificationHelper.java
+++ android/src/main/java/com/leetaehong/foregroundservice/NotificationHelper.java
@@ -228,19 +228,4 @@ class NotificationHelper {
         }
         return resourceId;
     }
-
-    public void scheduleAlarm(Context context) {
-        // Construct an intent that will execute the AlarmReceiver
-        Intent intent = new Intent(context.getApplicationContext(), NotificationReceiver.class);
-        // Create a PendingIntent to be triggered when the alarm goes off
-        final PendingIntent pIntent = PendingIntent.getBroadcast(context, NotificationReceiver.REQUEST_CODE,
-                intent, PendingIntent.FLAG_UPDATE_CURRENT);
-        // Setup periodic alarm every every half hour from this point onwards
-        long firstMillis = System.currentTimeMillis(); // alarm is set right away
-        AlarmManager alarm = (AlarmManager) context.getSystemService(Context.ALARM_SERVICE);
-        // First parameter is the type: ELAPSED_REALTIME, ELAPSED_REALTIME_WAKEUP, RTC_WAKEUP
-        // Interval can be INTERVAL_FIFTEEN_MINUTES, INTERVAL_HALF_HOUR, INTERVAL_HOUR, INTERVAL_DAY
-        alarm.setInexactRepeating(AlarmManager.RTC_WAKEUP, firstMillis,
-                AlarmManager.INTERVAL_FIFTEEN_MINUTES, pIntent);
-    }
 }
